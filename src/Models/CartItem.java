9be999dc5b4b186bcabb0b8b6ee91d86a8c42e7e
Vishal Ambhore <you@example.com>
2020-12-18 22:08:34 +0530

    created models for skuitem and cart item

--- src/Models/CartItem.java
+++ src/Models/CartItem.java
@@ -1,4 +1,28 @@
 package Models;
 
 public class CartItem {
+
+    int quantity;
+    SKUItem itemSKU;
+
+    public CartItem(int quantity, SKUItem itemSKU) {
+        this.quantity = quantity;
+        this.itemSKU = itemSKU;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public SKUItem getItemSKU() {
+        return itemSKU;
+    }
+
+    public void setItemSKU(SKUItem itemSKU) {
+        this.itemSKU = itemSKU;
+    }
 }
--- src/Models/SKUItem.java
+++ src/Models/SKUItem.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class SKUItem {
+    String SKU;
+    int price;
+
+    public SKUItem(String SKU,int price){
+        this.SKU=SKU;
+        this.price=price;
+    }
 }
